79e356e3a3dcf5c7d4946f0c4ffd133d20f2584a
rajan-gith <rajan.prajapati@berylsystems.com>
2019-05-31 18:37:55 +0530

    types of employees to records association added where record row contails different type of eployees

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,5 @@
 class Employee < ApplicationRecord
-  has_many :records
+  has_many :super_visor_records, class_name: "Record", foreign_key: "super_visor_id"
+  has_many :agent_records, class_name: "Record", foreign_key: "agent_id"
+  belongs_to :role
 end
